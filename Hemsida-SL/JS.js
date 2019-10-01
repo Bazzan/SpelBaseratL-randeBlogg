4bfc40868d82d6dd5df561311f7af51bfc1f09e4
Bazzan <sebake01@gmail.com>
2019-10-01 14:21:41 +0200

    Yoo

---
+++ Hemsida-SL/JS.js
@@ -0,0 +1,21 @@
+window.onload = function(){
+
+
+OmMig = function(){
+    window.location="OmMig.html";
+
+}
+
+Uppgift3 = function(){
+    window.location="Quiz-uppgift3.html";
+}
+
+Uppgift5 = function(){
+    window.location="Uppgift5.html";
+}
+
+Tillbaka = function(){
+    window.location="Index.html";
+}
+
+}
